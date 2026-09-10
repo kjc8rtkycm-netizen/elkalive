ed3d4c5b7c6c22392324d33f6af99e37ee32320d
elkalive <kjc8rtkycm@privaterelay.appleid.com>
2026-09-10 22:20:41 +0300

    Create turn-config.js

---
+++ turn-config.js
@@ -0,0 +1,9 @@
+window.METERED_TURN_CONFIG = {
+  urls: [
+    'turn:openrelay.metered.ca:80',
+    'turn:openrelay.metered.ca:443?transport=tcp',
+    'turns:openrelay.metered.ca:443?transport=tcp'
+  ],
+  username: 'openrelayproject',
+  credential: 'openrelayproject'
+};

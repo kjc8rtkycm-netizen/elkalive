47cf10b6824083f5e7b793f2980c189c2ac6068a
elkalive <kjc8rtkycm@privaterelay.appleid.com>
2026-09-11 08:41:32 +0300

    Delete turn-config.js

--- turn-config.js
+++
@@ -1,9 +0,0 @@
-window.METERED_TURN_CONFIG = {
-  urls: [
-    'turn:openrelay.metered.ca:80',
-    'turn:openrelay.metered.ca:443?transport=tcp',
-    'turns:openrelay.metered.ca:443?transport=tcp'
-  ],
-  username: 'openrelayproject',
-  credential: 'openrelayproject'
-};
